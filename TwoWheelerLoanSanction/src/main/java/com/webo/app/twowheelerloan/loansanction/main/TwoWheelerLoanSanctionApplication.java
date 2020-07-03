a1c07c70417601cbdb3540f7682423037759c037
pradeep8888 <pradipandhare38@gmail.com>
2020-07-03 09:07:43 +0530

    initial commit

---
+++ TwoWheelerLoanSanction/src/main/java/com/webo/app/twowheelerloan/loansanction/main/TwoWheelerLoanSanctionApplication.java
@@ -0,0 +1,13 @@
+package com.webo.app.twowheelerloan.loansanction.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoWheelerLoanSanctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoWheelerLoanSanctionApplication.class, args);
+	}
+
+}
